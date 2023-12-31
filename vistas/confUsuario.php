14a49041b09e63503fe4ef7f716a739446b4eba7
cris223511 <cris_antonio2001@hotmail.com>
2023-12-31 09:09:32 -0500

    [Christopher] más correcciones en locales

--- vistas/confUsuario.php
+++ vistas/confUsuario.php
@@ -61,7 +61,7 @@ if (!isset($_SESSION["nombre"])) {
                   </div>
                   <div class="form-group col-lg-6 col-md-6 col-sm-6 col-xs-12">
                     <label>Local principal(*):</label>
-                    <select id="idlocal" name="idlocal" class="form-control selectpicker" data-live-search="true" required onchange="actualizarRUC()">
+                    <select id="idlocal" name="idlocal" class="form-control selectpicker" data-live-search="true" onchange="actualizarRUC()" required>
                     </select>
                   </div>
                   <div class="form-group col-lg-6 col-md-6 col-sm-6 col-xs-12">
